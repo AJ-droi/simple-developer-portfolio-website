8eda096e05ed43a2626a5930a6d08b83e292cd36
Ajiri Osiobe <ajiriosiobe@gmail.com>
2023-01-14 19:48:46 +0100

    update v3.2

--- components/Projects.js
+++ components/Projects.js
@@ -12,7 +12,6 @@ export default function Projects() {
       {/* Grid starts here */}
       <div className="bg-[#F1F1F1] dark:bg-gray-900">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
-          <h2>Projects to be uploaded soon</h2>
           {userData.projects.map((proj, idx) => (
             <ProjectCard
               title={proj.title}
