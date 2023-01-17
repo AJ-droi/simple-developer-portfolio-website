fc3f60cd8d241622282a25b907f1d0a231f21ebe
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-17 02:09:09 +0100

    navbar fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,19 +3,31 @@ import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 import userData from "@constants/data";
-
+import {VscThreeBars} from "react-icons/vsc"
+import {HiXMark} from "react-icons/hi2"
 export default function Navbar() {
   const router = useRouter();
   console.log(router.asPath);
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [nav, setNav] = useState(0)
+  const [show, setShow] = useState(false);
+
 
   useEffect(() => {
     setMounted(true);
+    function handleResize() {
+      setNav(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return (
-    <div className="  mx-auto px-4 py-10 md:py-20 fixed z-50 bg-[#0C034B] w-[100%]  dark:bg-[#0C034]">
+    <div>
+    {nav > 480  ? <div className="  mx-auto px-4 py-10 fixed z-50 bg-[#111827] w-[100%]  dark:bg-[#111827]">
       <div className="flex  md:flex-row justify-between items-center">
         <div className="flex flex-col">
           <Link href="/">
@@ -159,13 +171,13 @@ export default function Navbar() {
         <div className="space-x-4 flex flex-row items-center">
         <a
               href={userData.socialLinks.github}
-              className="text-base font-normal text-gray-600 dark:text-gray-300"
+              className="text-base font-normal text-gray-600 dark:text-gray-300 "
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
             </a>
           <a
             href={userData.socialLinks.twitter}
-            className="text-base font-normal text-gray-600 dark:text-gray-300"
+            className="text-base font-normal text-gray-600 dark:text-gray-300 "
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -180,7 +192,7 @@ export default function Navbar() {
           </a>
           <a
             href={userData.socialLinks.linkedin}
-            className="text-base font-normal text-gray-600 dark:text-gray-300"
+            className="text-base font-normal text-gray-600 dark:text-gray-300 "
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -227,7 +239,7 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-      <div className="space-x-8 block flex flex-col mt-4 text-center h-[20vh] md:flex-row">
+      <div className="space-x-8 block flex flex-col mt-4 text-center h-[0vh] md:flex-row">
       <Link
           href="/"
           className="text-base font-normal text-gray-100  dark:text-gray-300 pl-[33px] md:pl-[0px]"
@@ -258,7 +270,222 @@ export default function Navbar() {
         >
           Contact
         </Link>
+      </div> 
+    </div> : <div className="  mx-auto px-4 py-10 fixed z-50 bg-[#111827] w-[100%]  dark:bg-[#111827]">
+      <div className="flex  md:flex-row justify-between items-center">
+        <div className="flex flex-col">
+          <Link href="/">
+            <h1 className="font-semibold text-xl text-gray-100 dark:text-gray-300">
+              {userData.name}
+            </h1>
+            <p className="text-base font-light text-gray-100 dark:text-gray-300">
+              {userData.designation}
+            </p>
+          </Link>
+        </div>
+
+        <div className="space-x-8 hidden md:block">
+        <Link
+            href="/"
+            className={`text-base text-[#fff] ${
+              router.asPath === "/about"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Home{" "}
+            {router.asPath === "/about" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/about"
+            className={`text-base text-[#fff] ${
+              router.asPath === "/about"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            About{" "}
+            {router.asPath === "/about" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/projects"
+            className={`text-base text-[#fff]  ${
+              router.asPath === "/projects"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Projects
+            {router.asPath === "/projects" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/experience"
+            className={`text-base text-[#fff] ${
+              router.asPath === "/experience"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Experience{" "}
+            {router.asPath === "/experience" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+          <Link
+            href="/contact"
+            className={`text-base text-[#fff] ${
+              router.asPath === "/contact"
+                ? "text-gray-800 font-bold dark:text-gray-400"
+                : "text-gray-600 dark:text-gray-300 font-normal "
+            }`}
+          >
+            Contact
+            {router.asPath === "/contact" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-down inline-block h-3 w-3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            )}
+          </Link>
+        </div>
+
+        <div className="space-x-4 flex flex-row items-center">
+          {!show ?<a onClick={() => setShow(!show)}><VscThreeBars className="text-[#fff] md:hidden" /></a> :<a onClick={() => setShow(!show)}><HiXMark className="text-[#fff] md:hidden" /></a>}
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-10 h-10 p-3 rounded focus:outline-none"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                stroke="currentColor"
+                className="w-4 h-4 text-yellow-500 dark:text-yellow-500"
+              >
+                {theme === "dark" ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
+     {show ?<div className="space-x-8  flex flex-col mt-4 text-center h-[22vh] md:hidden">
+      <Link
+          href="/"
+          className="text-base font-normal text-gray-100  dark:text-gray-300 pl-[33px] md:pl-[0px]"
+          onClick={() =>setShow(false)}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="text-base font-normal text-gray-100 my-[2%] md:my-[0%] dark:text-gray-300"
+          onClick={() =>setShow(false)}
+        >
+          About
+        </Link>
+        <Link
+          href="/projects"
+          className="text-base font-normal text-gray-100 my-[2%] md:my-[0%] dark:text-gray-300"
+          onClick={() =>setShow(false)}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/experience"
+          className="text-base font-normal text-gray-100 my-[2%] md:my-[0%] dark:text-gray-300"
+          onClick={() =>setShow(false)}
+        >
+          Experience
+        </Link>
+        <Link
+          href="/contact"
+          className="text-base font-normal text-gray-100 my-[2%] md:my-[0%] dark:text-gray-300"
+          onClick={() =>setShow(false)}
+        >
+          Contact
+        </Link>
+      </div> : <div> </div>}
+    </div>}
     </div>
   );
 }
